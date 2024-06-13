9508ebe9892ee1ed62f8d5e4fd2a94f03f63fcef
Sezer <s.y.sezeryardim@hotmail.com>
2024-06-13 12:41:36 +0300

    Header component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppHeader from "@components/Header/AppHeader.vue";
+</script>
 
-<template></template>
+<template>
+  <app-header></app-header>
+</template>
 
 <style scoped></style>
---
+++ src/components/Header/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <Menubar>
+    <template #start>
+      <Button label="Horse Racing" text />
+    </template>
+    <template #end>
+      <div class="flex align-items-center gap-2">
+        <Button label="Generate Program" />
+        <Button label="Start / Pause" />
+      </div>
+    </template>
+  </Menubar>
+</template>
+
+<script setup lang="ts">
+import Button from "primevue/button";
+import Menubar from "primevue/menubar";
+</script>
