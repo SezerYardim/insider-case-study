9e00940b185f50470d842123022be818ccd78e69
Sezer <s.y.sezeryardim@hotmail.com>
2024-06-13 21:56:35 +0300

    vuex initial

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import "./style.css";
 import App from "./App.vue";
 import PrimeVue from "primevue/config";
 import "primevue/resources/themes/aura-light-green/theme.css";
+import { store } from "./stores/store";
 
 const app = createApp(App);
 app.use(PrimeVue);
+app.use(store);
 
 app.mount("#app");
